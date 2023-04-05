84f04921dd8fe427d6abdfcf8dff7b9005e5cb22
Andrew Keith <keith.andrew.j@gmail.com>
2023-04-05 15:26:47 +0800

    Update main.py

--- textanalysis/main.py
+++ textanalysis/main.py
@@ -1,6 +1,6 @@
 import os
 from textanalysis import analysis
 
-path = os.path.join(os.getcwd(), 'data', 'policies', 'australia_defense.pdf'))
+path = os.path.join(os.getcwd(), 'data', 'policies', 'australia_defense.pdf')
 temp = analysis.extract_pdfs(path)
 df, fig = analysis.analyze_corpus(temp)
