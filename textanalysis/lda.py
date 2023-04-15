298557673628aab0e1a42b283676eb39cf7119bd
ajkeith <keith.andrew.j@gmail.com>
2023-04-15 14:35:24 +0800

    basic LDA working (small sample)

--- textanalysis/lda.py
+++ textanalysis/lda.py
@@ -1,16 +1,12 @@
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.decomposition import NMF, LatentDirichletAllocation
-
-# https://stackoverflow.com/questions/65113929/how-sklearn-latent-dirichlet-allocation-really-works
-# TODO: LDA
-
 from sklearn.feature_extraction import text
 from sklearn.decomposition import LatentDirichletAllocation
+from cleantext import clean
 import os
 
+# set parameters
 n_samples = 2000
 n_features = 1000
-n_components = 5
+n_components = 3
 n_top_words = 10
 
 # load text files
@@ -20,24 +16,32 @@ filenames = []
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     # if (filename.endswith("defense.txt") or filename.endswith("governance.txt")) and not filename.startswith("indonesia"): 
-    if filename.endswith("governance.txt") and not filename.startswith("indonesia"): 
+    if filename.endswith("governance.txt") and not (filename.startswith("gpai") or filename.startswith("indonesia")): 
         with open(os.path.join(os.getcwd(), 'data', 'texts', filename), 'r', encoding='utf-8') as f:
-            texts.append(f.read().replace('\n', ''))
+            clean_text = clean(f.read().replace('\n', ''),
+                               no_line_breaks=True,
+                               no_urls=True,
+                               no_digits=True,
+                               no_emails=True,
+                               no_phone_numbers=True,
+                               no_numbers=True)
+            texts.append(clean_text)
             filenames.append(filename)
 len(texts)
 filenames
 
 def print_top_words(model, feature_names, n_top_words):
+    """
+    Print the top words for each topic.
+    """
     for topic_idx, topic in enumerate(model.components_):
         message = "Topic #%d: " % topic_idx
         message += " ".join([feature_names[i]
                              for i in topic.argsort()[:-n_top_words - 1:-1]])
         print(message)
     print()
-    
 
-#create a count vectorizer using the sklearn CountVectorizer which has some useful features
-
+# build LDA model
 stop_words_custom = text.ENGLISH_STOP_WORDS.union(['australia', 
                                             'brunei', 
                                             'cambodia', 
@@ -48,7 +52,14 @@ stop_words_custom = text.ENGLISH_STOP_WORDS.union(['australia',
                                             'philippines', 
                                             'singapore', 
                                             'thailand', 
-                                            'vietnam'])
+                                            'vietnam',
+                                            'viet',
+                                            'nam',
+                                            '<number>',
+                                            '<email>',
+                                            '<url>',
+                                            '<phone>',
+                                            '00'])
 tf_vectorizer = text.CountVectorizer(max_df=0.95, min_df=2,
                                 max_features=n_features,
                                 stop_words=list(stop_words_custom))
@@ -57,11 +68,14 @@ lda = LatentDirichletAllocation(n_components=n_components, max_iter=5,
                                 learning_offset=50.,
                                 random_state=0)
 
+# classify topics
 X = texts
 vectorizedX = tf_vectorizer.fit_transform(X)
 lda.fit(vectorizedX)
 print("\nTopics in LDA model:")
 tf_feature_names = tf_vectorizer.get_feature_names_out()
 print_top_words(lda, tf_feature_names, n_top_words)
-[lda.transform(tf_vectorizer.transform([t])).max() for t in texts]
-lda.transform(tf_vectorizer.transform(["This is a strategy documenta about AI manufacturing"])).max()
+topics = [lda.transform(tf_vectorizer.transform([t])).argmax() for t in texts]
+probs = [lda.transform(tf_vectorizer.transform([t])).max() for t in texts]
+topic_summary = list(zip(filenames, topics, probs))
+print(*topic_summary, sep='\n')
