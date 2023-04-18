ebf1cfdc0b63500c151cd1d731910e17885b88b3
ajkeith <keith.andrew.j@gmail.com>
2023-04-18 17:04:14 +0800

    classify thai governance text

--- textanalysis/main.py
+++ textanalysis/main.py
@@ -2,8 +2,9 @@ import os
 from textanalysis import analysis
 import pandas as pd
 from cleantext import clean # text cleaning
+from transformers import pipeline # nlp models
 
-# full loop
+# classify english texts
 dir_path = os.fsencode(os.path.join(os.getcwd(), 'data', 'texts'))
 texts, filenames, languages = analysis.load_texts(dir_path)
 sclass, zclass = analysis.build_nlp_pipelines()
@@ -18,19 +19,16 @@ for text, filename, language in zip(texts, filenames, languages):
         figs.append(fig)
 figs[0].show()
 
-# exploration
-from transformers import pipeline # nlp models
+# classify indonesia governance text
 sclass = pipeline(task="sentiment-analysis")
-# zclass = pipeline(model="facebook/bart-large-mnli")
 iclass = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
 candidate_labels_id = ["perkembangan", "pengendalian", "promosi"]
-df_id = analysis.analyze_text(texts[2], 'id_test.pkl', 
+df_id = analysis.analyze_text(texts[2], 'indonesia_governance.pkl', 
                            sclass, iclass, candidate_labels, 
                            step=500, multi_label=True)
 fig_id = analysis.plot_nlp(df_id, 'indonesia (English Topics)', 'governance')
 fig_id.show()
-
-df_id_lbl = analysis.analyze_text(texts[2], 'id_test_id_lbl.pkl', 
+df_id_lbl = analysis.analyze_text(texts[2], 'indonesia_governance_id.pkl', 
                            sclass, iclass, candidate_labels_id, 
                            step=500, multi_label=True)
 fig_id_lbl = analysis.plot_nlp(df_id_lbl, 'indonesia (Bahasa Topics)', 'governance')
@@ -38,32 +36,23 @@ fig_id_lbl.show()
 
 # classify thai governance text
 dir_path = os.fsencode(os.path.join(os.getcwd(), 'data', 'texts'))
-with open(os.path.join(os.getcwd(), 'data', 'texts', 'thai_governance_thai.txt'), 'r', encoding='utf-8') as f:
-    raw_text = f.read().replace('\n', '')
-    clean_text = clean(raw_text,
-                                no_line_breaks=True,
-                                no_urls=True,
-                                no_digits=True,
-                                no_emails=True,
-                                no_phone_numbers=True,
-                                no_numbers=True)
-    text = clean_text
+with open(os.path.join(os.getcwd(), 'data', 'texts', 'thailand_governance_thai.txt'), 'r', encoding='utf-8') as f:
+    text_th = f.read().replace('\n', '')
 candidate_labels = ["development", "control", "promotion"]
 candidate_labels_th = ["การพัฒนา", "การควบคุม", "การส่งเสริม"]
-df_th_en = analysis.analyze_text(texts[2], 'id_test.pkl', 
+df_th_en = analysis.analyze_text(text_th, 'thailand_governance_th_en.pkl', 
                            sclass, iclass, candidate_labels, 
                            step=500, multi_label=True)
-fig_id = analysis.plot_nlp(df_id, 'indonesia (English Topics)', 'governance')
-fig_id.show()
+fig_th_en = analysis.plot_nlp(df_th_en, 'thailand (English Topics)', 'governance')
+fig_th_en.show()
 
-df_id_lbl = analysis.analyze_text(texts[2], 'id_test_id_lbl.pkl', 
-                           sclass, iclass, candidate_labels_id, 
+df_th_th = analysis.analyze_text(text_th, 'thailand_governance_th_th.pkl', 
+                           sclass, iclass, candidate_labels_th, 
                            step=500, multi_label=True)
-fig_id_lbl = analysis.plot_nlp(df_id_lbl, 'indonesia (Bahasa Topics)', 'governance')
-fig_id_lbl.show()
+fig_th_th = analysis.plot_nlp(df_th_th, 'thailand (Thai Topics)', 'governance') 
+fig_th_th.show()
 
-
-
-df_multi.groupby('Label').mean().sort_values('Score', ascending=False)
+# table summary
+df_id.groupby('Label').mean().sort_values('Score', ascending=False)
 for f in figs:
     f.show()
