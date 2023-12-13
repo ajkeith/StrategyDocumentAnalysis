21661bcd9bd19110181b88d0a1aa5fc453789d42
ajkeith <keith.andrew.j@gmail.com>
2023-12-13 13:38:26 +0800

    add more basic tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -10,6 +10,19 @@ class TestExistence(unittest.TestCase):
     def test_upper(self):
         self.assertEqual(self.text.dtypes[2].name, 'float64')
 
+    def test_directory(self):
+        self.assertEqual(os.path.exists(os.path.join(os.getcwd(), 'data', 'texts')), True)
+        dir_path = os.fsencode(os.path.join(os.getcwd(), 'data', 'texts'))
+        texts, filenames, languages = ta.load_texts(dir_path)
+        self.assertEqual(len(texts), 7) 
+        self.assertEqual(len(filenames), 7)
+        self.assertEqual(len(languages), 7)
+
+    def test_pipeline(self):
+        sclass, zclass = ta.build_nlp_pipelines()
+        self.assertEqual(sclass.task, 'sentiment-analysis')
+        self.assertEqual(zclass.task, 'zero-shot-classification')
+
     def tearDown(self):
         self.text = None
 
